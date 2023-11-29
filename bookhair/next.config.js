1f509cd31ae967bace55006d317c815f05704d3d
abde fakallah <senyoupl@icloud.com>
2023-11-29 12:26:06 +0100

    fix: hot reloading issue on container

--- bookhair/next.config.js
+++ bookhair/next.config.js
@@ -2,6 +2,13 @@
 //const nextConfig = {}
 
 module.exports = {
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 800,
+            aggregateTimeout: 300,
+        }
+        return config
+    },
     experimental: {
         serverActions: true,
       },
